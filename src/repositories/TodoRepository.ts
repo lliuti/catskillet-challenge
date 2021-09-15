1031b0c7cd05cdecb42c714f35228f00ce5cbec4
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 15:42:00 -0300

    Adding findByTitle function to TodoRepository

--- src/repositories/TodoRepository.ts
+++ src/repositories/TodoRepository.ts
@@ -2,6 +2,11 @@ import { EntityRepository, Repository } from "typeorm";
 import { Todo } from "../entities/Todo";
 
 @EntityRepository(Todo)
-class TodoRepository extends Repository<Todo> {}
+class TodoRepository extends Repository<Todo> {
+  async findByTitle(title: string): Promise<Todo> {
+    const todo = await this.findOne({ title: title });
+    return todo;
+  }
+}
 
 export { TodoRepository };
