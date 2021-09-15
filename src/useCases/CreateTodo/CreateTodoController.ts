e63eb0fe3bb7420442d97648f46c4482b3bae378
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 15:42:40 -0300

    Specifying which repository the controller should call

--- src/useCases/CreateTodo/CreateTodoController.ts
+++ src/useCases/CreateTodo/CreateTodoController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { TodoRepository } from "../../repositories/TodoRepository";
 import { CreateTodoUseCase } from "./CreateTodoUseCase";
 
 class CreateTodoController {
   async handle(request: Request, response: Response) {
     const { title, description, done } = request.body;
 
-    const createTodoUseCase = new CreateTodoUseCase();
+    const todoRepository = new TodoRepository();
+    const createTodoUseCase = new CreateTodoUseCase(todoRepository);
 
     try {
       await createTodoUseCase.execute({ title, description, done });
