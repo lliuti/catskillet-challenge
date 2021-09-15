2bd5bc764b0a1c4a28fd7030a6414ad299768e47
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 11:17:28 -0300

    Creating API documentation with SwaggerUI

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerConfig from "./swagger.json";
 import "./database/index";
 import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 app.use(routes);
 
 export { app };
