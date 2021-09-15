d4db02282b90892737d34c4b9dedc3653fcea7a1
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 16:10:49 -0300

    Specifying repository to ListTodoController

--- src/useCases/ListTodo/ListTodoController.ts
+++ src/useCases/ListTodo/ListTodoController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import { TodoRepository } from "../../repositories/TodoRepository";
 import { ListTodoUseCase } from "./ListTodoUseCase";
 
 class ListTodoController {
   async handle(request: Request, response: Response) {
-    const listTodoUseCase = new ListTodoUseCase();
+    const todoRepository = new TodoRepository();
+    const listTodoUseCase = new ListTodoUseCase(todoRepository);
     try {
       const list = await listTodoUseCase.execute();
       return response.status(200).json(list);
