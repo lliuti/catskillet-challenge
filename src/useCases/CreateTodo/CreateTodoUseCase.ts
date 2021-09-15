e229a11e3534bd439b09b91e3e70ddfca5a1ae31
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 15:45:04 -0300

    Fixing CreateTodoUseCase to pass the tests
    - Verifying if the repository has a property type of "MemoryRepository". I did this because i didnt find a way of separating the repositories (in-memory and database) for the tests. So, if I didnt add this property, everytime I tried to test, the tests would look for a connection with the database
    
    - Calling the function "findByTitle" so you cant create more than 1 Todo with the same title

--- src/useCases/CreateTodo/CreateTodoUseCase.ts
+++ src/useCases/CreateTodo/CreateTodoUseCase.ts
@@ -1,21 +1,35 @@
 import { getCustomRepository } from "typeorm";
-import { TodoRepository } from "../../repositories/TodoRepository";
 import { ICreateTodoDTO } from "./ICreateTodoDTO";
 
 class CreateTodoUseCase {
+  private todoRepository;
+
+  constructor(customRepository) {
+    if (customRepository.type === "MemoryRepository") {
+      this.todoRepository = customRepository;
+    } else {
+      this.todoRepository = getCustomRepository(customRepository);
+    }
+  }
+
   async execute({
     title,
     description,
     done = false,
   }: ICreateTodoDTO): Promise<void> {
-    const todoRepositories = getCustomRepository(TodoRepository);
-    const todo = todoRepositories.create({
+    const titleAlreadyExists = await this.todoRepository.findByTitle(title);
+
+    if (titleAlreadyExists) {
+      throw new Error("There is already a registered Todo with this title");
+    }
+
+    const todo = this.todoRepository.create({
       title,
       description,
       done,
     });
 
-    await todoRepositories.save(todo);
+    await this.todoRepository.save(todo);
   }
 }
 
