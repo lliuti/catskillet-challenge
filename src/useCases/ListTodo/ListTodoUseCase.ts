58b57e2042f468442ff068e68fe344567e185221
Lucas Liuti <dev.lliuti@gmail.com>
2021-09-15 10:38:48 -0300

    Throwing error if no Todo is found in listing method

--- src/useCases/ListTodo/ListTodoUseCase.ts
+++ src/useCases/ListTodo/ListTodoUseCase.ts
@@ -6,6 +6,11 @@ class ListTodoUseCase {
   async execute(): Promise<Todo[]> {
     const todoRepository = getCustomRepository(TodoRepository);
     const list = await todoRepository.find();
+
+    if (list.length < 1) {
+      throw new Error("Could not find any Todo");
+    }
+
     return list;
   }
 }
